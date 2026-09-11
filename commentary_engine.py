d05e2b52e69d9d076f2beb98a95ed8d0d17b5d28
RiddhimanDeveloper <riddhimanprem17@gmail.com>
2026-09-11 14:18:24 +0400

    Implement cricket commentary generation
    Added functionality to generate cricket commentary using a language model.

--- commentary_engine.py
+++ commentary_engine.py
@@ -1,1 +1,13 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+load_dotenv()
 
+llm=ChatOpenAI()
+
+Template= ChatPromptTemplate([('System',"You are a legendary, energetic cricket commentator. "
+        "Describe this event in a sentence or 2, "
+        "using classic cricket terminology."),('user','{event_details}')])
+
+commentary_chain = prompt | llm
